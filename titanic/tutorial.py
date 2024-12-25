3cf60d75e20c76b779f6deaa15da42f211d97530
Nindya K. Desyanasari <nkdesyanasari@gmail.com>
2024-12-25 14:26:04 +0100

    EDA (not yet fully cleaned)

---
+++ titanic/tutorial.py
@@ -0,0 +1,7 @@
+while True:
+    name = input("Siapa ya jodoh Tiszak? (Tulis disini): ")
+    if name.lower() == 'yono':  # Check if the input matches 'yono' (case-insensitive)
+        print("Selamat menuju szurga bersama Yono!")
+        break  # Exit the loop when the correct name is entered
+    else:
+        print("Gagal, masuk neraka. Gabisa masuk syurga sebelum tulis yang bener HAHAHAHA")
